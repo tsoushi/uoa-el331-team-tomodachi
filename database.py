33160f3bb67476afcba9e0893ff979d5fea550bb
tsoushi <79128170+tsoushi@users.noreply.github.com>
2024-05-20 15:54:49 +0900

    TextFileに対するCRUDを実装

--- database.py
+++ database.py
@@ -27,3 +27,24 @@ def get_txt_file(filename: str) -> Optional[domain.TextFile]:
     con.close()
     return domain.TextFile(row[0], row[1]) if row else None
 
+def get_all_txt_files() -> list[domain.TextFile]:
+    con = get_con()
+    cursor = con.cursor()
+    cursor.execute('SELECT * FROM txt_file')
+    rows = cursor.fetchall()
+    con.close()
+    return [domain.TextFile(row[0], row[1]) for row in rows]
+
+def update_txt_file(text_file: domain.TextFile) -> domain.TextFile:
+    con = get_con()
+    con.execute('UPDATE txt_file SET content = ? WHERE filename = ?', (text_file.content, text_file.filename))
+    con.commit()
+    con.close()
+    return text_file
+
+def delete_txt_file(filename: str) -> str:
+    con = get_con()
+    con.execute('DELETE FROM txt_file WHERE filename = ?', (filename,))
+    con.commit()
+    con.close()
+    return filename
--- main.py
+++ main.py
@@ -10,22 +10,73 @@ app = flask.Flask(__name__)
 
 
 @app.route('/text-file', methods=['POST'])
-def create():
+def create_text_file():
     data = request.get_json()
     file_name = data['fileName']
     content = data['content']
 
     text_file = database.create_txt_file(domain.TextFile(file_name, content))
-    return jsonify(api_response.text_file.post(text_file))
+    return jsonify({
+        'message': 'success',
+        'textFile': {
+            'filename': text_file.filename,
+            'content': text_file.content
+        }
+    })
 
 @app.route('/text-file', methods=['GET'])
-def get():
+def get_text_file():
     data = request.get_json()
     file_name = data['fileName']
 
     text_file = database.get_txt_file(file_name)
-    return jsonify(api_response.text_file.get(text_file))
+    return jsonify({
+        'message': 'success',
+        'textFile': {
+            'filename': text_file.filename,
+            'content': text_file.content
+        }
+    })
 
+@app.route('/text-file', methods=['PUT'])
+def update_text_file():
+    data = request.get_json()
+    file_name = data['fileName']
+    content = data['content']
+
+    text_file = database.update_txt_file(domain.TextFile(file_name, content))
+    return jsonify({
+        'message': 'success',
+        'textFile': {
+            'filename': text_file.filename,
+            'content': text_file.content
+        }
+    })
+
+@app.route('/text-file', methods=['DELETE'])
+def delete_text_file():
+    data = request.get_json()
+    file_name = data['fileName']
+
+    text_file = database.delete_txt_file(file_name)
+    return jsonify({
+        'message': 'success',
+        'textFile': {
+            'filename': text_file.filename,
+            'content': text_file.content
+        }
+    })
+
+@app.route('/text-file/all', methods=['GET'])
+def get_all_text_files():
+    text_files = database.get_all_txt_files()
+    return jsonify({
+        'message': 'success',
+        'textFiles': [{
+            'filename': text_file.filename,
+            'content': text_file.content
+        } for text_file in text_files]
+    })
 
 if __name__ == '__main__':
 
