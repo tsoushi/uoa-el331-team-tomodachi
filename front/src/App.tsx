a038f05d64ef79e37e49685f123f4c88ff7cafcf
tsoushi <79128170+tsoushi@users.noreply.github.com>
2024-05-27 15:49:13 +0900

    フロントエンドの外枠を作成

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,61 @@
-import './App.css'
+import { useState } from 'react'
+import { css } from '@emotion/react'
+import styled from '@emotion/styled'
+
+import { FileManager } from './FileManager'
+
+const MenuBox = styled.ul`
+  display: flex;
+  justify-content: flex-start;
+  padding: 5px 10px;
+  margin: 10px;
+  border-radius: 10px;
+  list-style: none;
+
+  background-color: #f0f0f0;
+`
+
+
+const MenuItem = styled.li`
+  margin: 10px;
+  padding: 10px;
+  cursor: pointer;
+  &:hover {
+    background-color: #e0e0e0;
+  }
+`
+
+type SceneName = 'FileManager' | 'ExploratorySearch' | 'CompareQvsK' | 'ConsistencyKvsK'
+
+function MenuBar({ setScene }: { setScene: React.Dispatch<React.SetStateAction<SceneName>>}) {
+  
+  return (
+    <nav>
+      <MenuBox>
+        <MenuItem onClick={() => setScene('FileManager')}>File Manager</MenuItem>
+        <MenuItem onClick={() => setScene('ExploratorySearch')}>Exploratory Search</MenuItem>
+        <MenuItem onClick={() => setScene('CompareQvsK')}>Compare Q vs K</MenuItem>
+        <MenuItem onClick={() => setScene('ConsistencyKvsK')}>Consistency K vs K</MenuItem>
+      </MenuBox>
+    </nav>
+  )
+}
 
 function App() {
+  const [scene, setScene] = useState<SceneName>('FileManager')
 
   return (
-    <h1>AuthorShip analysis</h1>
+    <>
+      <MenuBar setScene={setScene}/>
+      <div css={css`
+        padding: 10px;
+      `}>
+        {scene === 'FileManager' && <FileManager />}
+        {scene === 'ExploratorySearch' && <h1>Exploratory Search</h1>}
+        {scene === 'CompareQvsK' && <h1>Compare Q vs K</h1>}
+        {scene === 'ConsistencyKvsK' && <h1>Consistency K vs K</h1>}
+      </div>
+    </>
   )
 }
 
